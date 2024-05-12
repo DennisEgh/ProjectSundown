de01429fdd3db9dc8276806801670ce195c7a046
Dennis Eghtedari <year_2000_@hotmail.com>
2024-05-12 16:21:35 +0100

    save

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,12 @@
+
 export default function Home() {
+  
   return (
     <section id="landing__page">
-     
+        <video autoPlay loop muted className="video">
+          <source src="" type="video/mp4" />
+        </video>
+      
     </section>
   );
 }
