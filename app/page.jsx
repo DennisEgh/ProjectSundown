d13b42d604b7335bbf71d0cd30d46e68b6b2bcf1
Dennis Eghtedari <year_2000_@hotmail.com>
2024-05-12 15:10:22 +0100

    cleaned file

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <section id="landing__page">
+      <div className="landing__page--greeting--container fade-in">
+        <h1 className="landing__page--title ">
+          The Stars Are Humanitys Birthright
+        </h1>
+        <p className="landing__page--para">
+          With the worlds most brilliant minds helping develop the latest in
+          interstellar technology along with the brave men and women to use
+          them, we take to the stars. Join us today and help colonize our Solar
+          System, Galaxy and Universe!
+        </p>
+      </div>
+    </section>
+  );
+}
